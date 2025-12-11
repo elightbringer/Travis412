4dd27d075afd64cd19ecdd0230ca3547bcd4fbc8
elightbringer <ethan.lightbringer@gmail.com>
2025-12-10 19:02:32 -0500

    Update SimpleCalculatorTest.java

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -26,4 +26,9 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.divide(6, 2), 3);
 	}
+
+	public void testDivideZero() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.divide(2, 0), 0);
+	}
 }
